0361975dfd2903460918d60697bacd4324b4b1b2
Tara Koomen <tarakoomen88@gmail.com>
2024-03-23 09:49:16 +1100

    First step completed of the simple airbnb app

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,23 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require 'faker'
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+3.times do
+  Flat.create!(
+    name: Faker::Company.name,
+    address: Faker::Address.full_address,
+    description: Faker::Lorem.sentence,
+    price_per_night: Faker::Commerce.price,
+    number_of_guests: Faker::Number.between(from: 1, to: 10)
+  )
+end
